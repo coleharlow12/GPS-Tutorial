fe74b90aa694db6bc6f15cd86c5c92188b6d47e7
coleharlow12 <33840536+coleharlow12@users.noreply.github.com>
2019-08-28 09:00:10 -0400

    Update Adafruit_GPS_Tutorial.ino

--- Adafruit_GPS_Tutorial.ino
+++ Adafruit_GPS_Tutorial.ino
@@ -2,9 +2,12 @@
 #include <GPSport.h>
 #include <NMEAGPS.h>
 
+//Includes the AltSoftSerial library so that an additional Serial port can be emulated using digital pins 8 and 9
 #include <AltSoftSerial.h>
 
-//This setups the Software Serial Port using pin 8 as the rx pin and pin 9 as the tx pin
+//Creates an AltSoftSerial Object known as mySerial on pins 8 and 9
+//Pin 8 is the rx
+//Pin 9 is the tx
 AltSoftSerial mySerial(8,9);
 
 //These variables are used to store the current and past longintude, latitude and Altitude
@@ -16,20 +19,19 @@ NMEAGPS gps; // This parses the GPS characters
 gps_fix fix; //This structure holds the GPS data
 
 void setup() {
-  //This command uses the included libraries to find the connected GPS and begin communications
-  //with it
-  gpsPort.begin(9600);
+  //This command starts the softserial port GPSSerial at a baudrate of 9600
+  GPSSerial.begin(9600);
   //This command starts the hardware serial at a baudrate of 57600
   Serial.begin(57600);
 }
 
 void loop() {
   //While there is GPS data available
-  while(gps.available(gpsPort))
+  while(gps.available(GPSSerial))
   {
     //Reads the data from the GPS
     fix = gps.read();
-
+    
     //If the received data appears to be valid GPS coordinates
     if(fix.valid.location)
     {
